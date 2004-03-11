5554a1967079f7788ec4627f6dba400642d84e8f
Urpo Lankinen <wwwwolf@iki.fi>
2004-03-11 18:57:40 +0000

    Skeletal "Emitter" thing is now here.

---
+++ neverblender/lib/Emitter.py
@@ -0,0 +1,22 @@
+#################################################################
+#
+# Emitter.py
+# For turning NWN meshes into MDL Emitters.
+#
+# part of the NeverBlender project
+# (c) The NeverBlender Contributors 2003
+# Distribute, modify and use however you please as long as you
+# retain this copyright notice and comply to the terms set forth in the
+# file COPYING. No warranty expressed or implied.
+# $Id$
+#
+#################################################################
+
+
+class Emitter:
+    def __init__(self, object=None):
+        pass
+
+    def __str__(self):
+        pass
+
